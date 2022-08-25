b58a3e54d138a6f2250017114e5d8ac0025a60ec
mohammad-davari-82 <mohammaddavarzani1998@gmail.com>
2022-08-25 09:32:03 +0430

    fixed a bug routing

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import React from 'react';
-import Layout from '../layout/layout';
 import Home from '../components/home/home';
 import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import Map from '../components/home/map/map';
+import Layout from '../layout/layout';
 const App = () => {
   return (
       <BrowserRouter>
-        <Layout>
-        </Layout>
+      <Layout></Layout>
+        <Routes>
+          <Route  path="/snapp"  element={<Map/>}/>
+        </Routes>
       </BrowserRouter>
   );
 }
